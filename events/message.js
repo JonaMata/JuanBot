9b2b2df883b6bfe5c4850336ac333c669139d28d
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2019-07-15 02:06:07 +0200

    Update to work with env

--- events/message.js
+++ events/message.js
@@ -3,7 +3,7 @@ module.exports = (client, msg) => {
   if (msg.author.bot) return;
   if(msg.content.indexOf(client.config.PREFIX) !== 0) return;
 
-  const args = msg.content.slice(client.config.prefix.length).trim().split(/ +/g);
+  const args = msg.content.slice(client.config.PREFIX.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   const cmd = client.commands.get(command);
   if (!cmd) return;
