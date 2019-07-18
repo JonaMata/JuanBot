ceb5c21fe5d70da9ffd2420211110046165aa4bb
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2019-07-18 14:37:42 +0200

    Added simple http server for uptime tracking

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const Config = process.env;
+const http = require('http');
 
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: Config.SENTRYURL });
@@ -19,4 +20,17 @@ Sharder.on('message', (message) => {
       shard.process.kill();
     })
   }
+});
+
+
+//Simple httpserver for uptime checking
+const requestHandler = (request, response) => {
+  response.end('JuanBot UP');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(Config.PORT, (err) => {
+  if(err) return console.log("Couldn't start http server, error: ". err);
+  console.log(`HTTP server listening on ${Config.PORT}`);
 });
